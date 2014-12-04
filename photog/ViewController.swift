d4c186f4438e510275dfb54fdebc4d27051490df
Ryan Valle <ryanvalle@me.com>
2014-12-03 22:22:17 -0800

    Initial Commit

---
+++ photog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photog
+//
+//  Created by Ryan Valle on 12/3/14.
+//  Copyright (c) 2014 RyanValle.me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
